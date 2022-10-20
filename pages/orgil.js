a1b56ba968c3676e1f4facc07538914f6007e2da
Orgil <orgio050505@gmail.com>
2022-10-20 22:11:04 +0800

    orgil push hiiv

--- pages/orgil.js
+++ pages/orgil.js
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function orgil() {
   return (
-    <div>orgil sain u</div>
+    <div class="text-black px-4 py-6 md:p-12 md:mx-6">
+      <h4 class="text-xl font-semibold mb-6">We are more than just a company</h4>
+        <p class="text-sm">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                  consequat.
+        </p>
+     </div>
   )
 }
