9a6b27896088ff1fa4351c59bac1842931d9f6a8
otgonbat <otgonbat20@gmail.com>
2023-02-08 15:11:20 +0800

    delete unneccesary fiels

--- pages/orgil.js
+++
@@ -1,9 +0,0 @@
-export default function orgil() {
-  return (
-    <div class="w-full h-full bg-gray-100 flex items-center justify-center relative">
-      <div class="rounded-full flex h-60 w-60 bg-purple-600 justify-center">
-        <div class=" backdrop-blur-lg drop-shadow-lg w-full h-1/2 absolute bottom-0 bg-white bg-opacity-20 "></div>
-      </div>
-    </div>
-  );
-}
--- pages/togsoo.js
+++
@@ -1,1 +0,0 @@
-//sain bainy sda min
