22f4de3896e485fde06992509d835dabf2fc3f2f
otgonbat <otgonbat20@gmail.com>
2023-02-28 09:07:29 +0800

    clear all

--- components/log_in.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function log_in() {
-  return (
-    <>
-      <div className="w-full h-full bg-red-500"> gal bainu</div>
-    </>
-  );
-}
--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import Log_in from "../components/log_in.js";
-
-export default function Home() {
-  return (
-    <>
-      <Log_in />
-    </>
-  );
-}
