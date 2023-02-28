c423a856d298f9d5d9544730f926dedf576785fc
otgonbat <otgonbat20@gmail.com>
2023-02-28 08:34:43 +0800

    gal2

---
+++ components/log_in.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function log_in() {
+  return (
+    <>
+      <div className="w-full h-full bg-red-500"> gal bainu</div>
+    </>
+  );
+}
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import Log_in from "../components/log_in.js";
+
+export default function Home() {
+  return (
+    <>
+      <Log_in />
+    </>
+  );
+}
